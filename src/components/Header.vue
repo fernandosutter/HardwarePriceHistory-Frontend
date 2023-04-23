225f516635c28bbaa020fd6d2efbbdf697e3ebfa
Fernando Sutter <fernando.sutter1@gmail.com>
2023-04-22 22:31:48 -0300

    Feito form com a busca aos produtos na API

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class="header">
-        <h1 class="header-text">HardwarePriceHistory</h1>
-    </header>
+  <header class="header">
+    <h1 class="header-text">HardwarePriceHistory</h1>
+  </header>
 </template>
 
 <script>
@@ -11,20 +11,25 @@ export default {
 </script>
 
 <style scoped>
-
-.header{
-  text-align: start; /* Alinhar texto para o inicio */
-  background-color: #333; /* Cor de fundo */
-  color: white; /* Cor do texto */
-  position: fixed; /* Fixar o cabeçalho na parte superior */
-  top: 0; /* Colocar o cabeçalho no topo da página */
-  left: 0; /* Alinhar o cabeçalho à esquerda */
-  width: 100%; /* Definir a largura máxima do cabeçalho */
+.header {
+  /* Alinhar texto para o inicio */
+  text-align: start;
+  /* Cor de fundo */
+  background-color: #333;
+  /* Cor do texto */
+  color: white;
+  /* Fixar o cabeçalho na parte superior */
+  position: fixed;
+  /* Colocar o cabeçalho no topo da página */
+  top: 0;
+  /* Alinhar o cabeçalho à esquerda */
+  left: 0;
+  /* Definir a largura máxima do cabeçalho */
+  width: 100%;
 }
 
-.header-text{
+.header-text {
   color: white;
   margin: 10px;
-}
-
-</style>
+  font-size: 16px;
+}</style>
--- src/components/ProductPrices.vue
+++ src/components/ProductPrices.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="hello">
-    <h1>asdasdasd</h1>
+  <div class="container">
+    <div class="box">
+      <b-field grouped>
+        <b-field label="Produto" expanded>
+          <b-autocomplete
+            id="campoProduto"
+            v-model="produtoSelecionado"
+            field="productName"
+            :data="produtoParaBusca"
+            icon="magnify"
+            placeholder="RTX"
+            :open-on-focus=true
+            @select="option => selecionarProduto(option)"
+            @typing="buscarProdutosAsync">
+          <template #empty>Nenhum produto foi encontrado</template>
+        </b-autocomplete>
+        </b-field>
+        <b-field label="Datas">
+        <b-datepicker placeholder="Clique para selecionar" v-model="datas" range>
+        </b-datepicker>
+      </b-field>
+    </b-field>
+    </div>
   </div>
 </template>
 
@@ -9,15 +30,46 @@ export default {
   name: 'ProductPrices',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      datas: [],
+      produtoSelecionado: '',
+      produtoIDSelecionado: 0,
+      produtoParaBusca: []
+    }
+  },
+  methods: {
+    selecionarProduto (option) {
+      this.produtoSelecionado = option.productName
+      this.produtoIDSelecionado = option.id
+    },
+    buscarProdutosAsync () {
+      this.produtoParaBusca = []
+      this.axios.get('https://localhost:7022/api/Product?name=' + this.produtoSelecionado)
+        .then(response => {
+          response.data.forEach(element => {
+            this.produtoParaBusca.push(element)
+          })
+        })
     }
   }
 }
 </script>
 
 <style scoped>
-.hello{
-  position: fixed;
-  top: 57px;
+.container {
+  padding-top: 44px;
+}
+
+label {
+  margin: 6px;
+}
+
+.campoProduto {
+  display: flex;
+  flex-grow: 2;
+}
+
+.campoData {
+  padding-top: 24px;
 }
 </style>
